bce9513bcb774847a962c82bd1b393c9db1936f7
Gannouni Doua <150908416+doaagannouni@users.noreply.github.com>
2023-12-11 12:10:36 +0100

    config

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -13,7 +13,7 @@ class CreatePasswordResetTokenTableee extends Migration
      */
     public function up()
     {
-        Schema::create('password_reset_token', function (Blueprint $table) {
+        Schema::create('password_reset_tokennn', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
@@ -27,6 +27,6 @@ class CreatePasswordResetTokenTableee extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_reset_token');
+        Schema::dropIfExists('password_reset_tokennn');
     }
 }
